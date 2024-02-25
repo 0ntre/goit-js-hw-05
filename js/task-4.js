6f90620ca502c745e2e5cd36775b8014e217ec34
0ntre <alexis19955991@gmail.com>
2024-02-25 19:08:04 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,8 +1,3 @@
-// Напиши стрілочну функцію getTotalBalanceByGender(users, gender), яка прийматиме два параметра:
-// перший параметр users — масив об’єктів користувачів,
-// другий параметр gender — рядок, що зберігає стать.
-// Функція має використовувати ланцюжок виклику методів та повертати загальний баланс користувачів (властивість balance), стать яких (властивість gender) збігається зі значенням параметра gender.
-
 const clients = [
   {
     name: 'Moore Hensley',
@@ -41,6 +36,10 @@ const clients = [
   },
 ];
 
-console.log(getTotalBalanceByGender(clients, 'male')); // 12053
+const getTotalBalanceByGender = (users, gender) =>
+  users
+    .filter(user => user.gender === gender)
+    .reduce((totalBalance, user) => totalBalance + user.balance, 0);
 
+console.log(getTotalBalanceByGender(clients, 'male')); // 12053
 console.log(getTotalBalanceByGender(clients, 'female')); // 8863
